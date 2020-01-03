4e08a60ddb9b4ceb2f3e7ade7858c337a32b1fc1
Huelse <huelse@oini.top>
2020-01-04 00:43:20 +0800

    Init vector with length and default
    Fix the version, requirements.txt and CMakelists.txt
    Add support with default vector

--- src/setup.py
+++ src/setup.py
@@ -22,7 +22,7 @@ ext_modules = [
 
 setup(
     name='seal',
-    version='3.3.1',
+    version='3.3.2',
     author='Huelse', 
     author_email='huelse@oini.top',
     description='Python wrapper for SEAL',
