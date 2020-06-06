7ffdd4af6b4d350232a075fd0e51f50807fa68d2
Huelse <5956877@qq.com>
2020-06-06 22:22:49 +0800

    add header file

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM ubuntu:19.10
+FROM ubuntu:18.04
 
 # Install binary dependencies
 RUN apt-get update && \
--- src/wrapper.cpp
+++ src/wrapper.cpp
@@ -3,6 +3,7 @@
 #include <pybind11/numpy.h>
 #include <pybind11/stl.h>
 #include <pybind11/stl_bind.h>
+#include <fstream>
 #include "seal/seal.h"
 #include "base64.h"
 
