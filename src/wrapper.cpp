d051125de15c51f854459206ecee6047f15f7cd4
max <huelse@qq.com>
2026-03-27 18:29:18 +0800

    Update version to 4.1.2.1

--- src/wrapper.cpp
+++ src/wrapper.cpp
@@ -18,7 +18,7 @@ PYBIND11_MAKE_OPAQUE(std::vector<std::int64_t>);
 PYBIND11_MODULE(seal, m)
 {
     m.doc() = "Microsoft SEAL for Python, from https://github.com/Huelse/SEAL-Python";
-    m.attr("__version__")  = "4.1.2";
+    m.attr("__version__")  = "4.1.2.1";
 
     py::bind_vector<std::vector<double>>(
         m, "VectorDouble", py::buffer_protocol(),
